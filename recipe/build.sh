6a1ad201f61fbfb7a88e68a2312eb2ab5c97d1aa
ViridianMelody <mlove@anaconda.com>
2024-08-26 13:59:09 -0600

    Remove unused $EXTRA_MANPAGES_OPT

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,6 @@ cmake ${CMAKE_ARGS} -D CMAKE_BUILD_TYPE=Release     \
       -D CMAKE_INSTALL_LIBDIR=lib     \
       -D BUILD_SHARED_LIBS=ON         \
       -D BUILD_EXAMPLES=OFF           \
-      $EXTRA_MANPAGES_OPT             \
       $SRC_DIR
 
 make -j${CPU_COUNT}
