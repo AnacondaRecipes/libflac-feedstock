048cae5b74bbb2d60c33a17445f8d24ac441fb64
ViridianMelody <mlove@anaconda.com>
2024-08-26 08:24:06 -0600

    Remove ppc64le sections

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,6 @@
 
 set -ex
 
-# The pandoc package is required for building the manpages,
-# but it is not available currently for ppc64le.
-
-if [[ $target_platform == linux-ppc64le ]]; then
-    EXTRA_MANPAGES_OPT="-D INSTALL_MANPAGES=off"
-fi
-
-
 cmake ${CMAKE_ARGS} -D CMAKE_BUILD_TYPE=Release     \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_INSTALL_LIBDIR=lib     \
